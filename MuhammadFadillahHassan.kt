194a2ef7b669673cf6d264e9942b6408fa8461d7
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-14 06:36:58 +0800

    Add files via upload

---
+++ MuhammadFadillahHassan.kt
@@ -0,0 +1,31 @@
+val nama: String = "Muhammad Fadillah Hassan"
+val nim: Long = 2010817110005
+val prodi: String = "Teknologi Informasi"
+val asal: String = "Banjarmasin"
+val umur: Int = 19
+val ip: Float = 3.77F
+val crush: Char = 'M'
+val hobi: String = "ngegame"
+val sifat: String = "\n 1. Gasuka susah \n 2. Orang baik kok :) \n 3. Mau yang simple \n 4. Mau simple tapi sukses :3\n"
+
+
+fun kenalan(){
+    println("Namaku $nama, dari Program Studi $prodi dengan NIM $nim, sekarang tinggal di $asal")
+}
+
+fun ip(){
+    println("IP semesterku yang lalu adalah $ip")
+}
+
+fun relate(){
+    println("Di umurku yang $umur ini, mungkin aku gapernah berpikir di masa depan aku mau jadi apa, " +
+            "hanya saja saya punya pasangan dengan inisial $crush yang selalu membuatku berpikir aku harus produktif:)")
+}
+
+fun hobi(){
+    println("Hobi? Mungkin kalau bisa dibilang hobiku adalah $hobi terus-terusan")
+}
+
+fun sifat(){
+    println("Untuk sifat, mungkin sifatku itu begini : $sifat")
+}
